d0923c909d583ae43f75f07e45adf32c43e8dd33
Shivam Singh <shivamsinghmmmut@gmail.com>
2020-05-27 20:08:14 +0530

    Commited BinarySearch Algorithm

---
+++ BinarySearch/BinarySearchAlgorithms.java
@@ -0,0 +1,45 @@
+package BinarySearch;
+
+import java.util.Scanner;
+
+public class BinarySearchAlgorithms {
+	public int BinarySearch(int arr[],int x)
+	{
+		int start=0;
+		int end=arr.length-1;
+		while(start<=end)
+		{
+			int mid=start+(end-start)/2;
+			if(arr[mid]==x) return mid;
+			else if(arr[mid]<x)
+			{start=mid+1;
+				
+			}
+			else
+			{
+				end=mid-1;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		BinarySearchAlgorithms obj=new BinarySearchAlgorithms();
+		System.out.println("Enter no of element in array");
+		int NoOfElementInArray=sc.nextInt();
+		int arr[]=new int[NoOfElementInArray];
+		for(int i=0;i<NoOfElementInArray;i++)
+		{
+			 arr[i]=sc.nextInt();
+			 System.out.println("enter next element");
+		}
+		System.out.println("enter element to be search");
+		int ElementToSearch=sc.nextInt();
+		int ElementPresentAt=obj.BinarySearch( arr, ElementToSearch );
+		System.out.println(ElementPresentAt);
+
+	}
+
+}
