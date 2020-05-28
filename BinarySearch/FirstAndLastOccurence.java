a9cd3909939ddf03235881b27da143ac78f0fd11
Shivam Singh <shivamsinghmmmut@gmail.com>
2020-05-28 20:26:52 +0530

    Program to Find First and last occurence

--- BinarySearch/FirstAndLastOccurence.java
+++ BinarySearch/FirstAndLastOccurence.java
@@ -30,6 +30,32 @@ public class FirstAndLastOccurence {
 		}
 		return res;
 	}
+	public int LastOcuurence(int arr[],int x)
+	{
+		int start =0;
+		int end =arr.length-1;
+		int res=-1;
+		while(start<=end)
+		{
+			int mid=start+(end-start)/2;
+			if (arr[mid]==x)
+			{
+				 res=mid;
+				start=mid+1;
+			}
+			else if(arr[mid]>x)
+			{
+				end=mid-1;
+			}
+			else
+			{
+				start=mid+1;
+			}
+			
+		}
+		return res;
+	}
+
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
@@ -46,7 +72,11 @@ public class FirstAndLastOccurence {
 		System.out.println("enter element to be search");
 		int ElementToSearch=sc.nextInt();
 		int ElementPresentAt=obj.FirstOcuurence( arr, ElementToSearch );
+		int LastElementOcuurence=sc.nextInt();
+		
+		 LastElementOcuurence=obj.LastOcuurence( arr, ElementToSearch );
 		System.out.println(ElementPresentAt);
+		System.out.println(LastElementOcuurence);
 		
 
 	}
